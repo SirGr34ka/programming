c83fbf9a7dfc96baad158746f88e733e51e61be4
dyo4 <dyo4@tpu.ru>
2024-03-13 12:00:04 +0700

    sequence error

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
+// 49
 
 void sum(std::vector<unsigned int> &res, std::vector<unsigned int> &nums1, std::vector<unsigned int> &nums2)
 {
     size_t size = nums2.size();
+    unsigned long long temp = nums1.back() + nums2.back();
+    unsigned int a = temp >> 31;
 
-    if ((nums1.back() + nums2.back()) >> 31)
+    res.clear();
+
+    if (temp >> 31)
     {
         res.resize(size + 1);
+        res.push_back(0);
     }
     else
     {
@@ -16,38 +22,48 @@ void sum(std::vector<unsigned int> &res, std::vector<unsigned int> &nums1, std::
 
     for (size_t i = 0; i < size; ++i)
     {
-        if ((nums1[i] + nums2[i]) >> 31)
+        temp = nums1[i] + nums2[i];
+        res[i] += temp & ~(1 << 32);
+
+        if (temp >> 32)
         {
-            res[i] += (nums1[i] + nums2[i]) & ~(1 << 31);
-            ++res[i + 1];
-        }
-        else
-        {
-            res[i] += nums1[i] + nums2[i];
+            res[i + 1] = 1;
         }
     }
 
     return;
 }
 
+void fibonacci_sequence(size_t num, std::vector<std::vector<unsigned int>> &elements)
+{
+    std::vector<unsigned int> res(1);
+
+    elements[0] = res;
+    elements[1] = res;
+
+    res[0] = 1;
+    elements[2] = res;
+
+    for (size_t i = 3; i <= num; ++i)
+    {
+        sum(res, elements[i - 2], elements[i - 1]);
+        elements[i] = res;
+    }
+    
+    return;
+}
+
 int main(int, char**)
 {
-    unsigned int input_1;
-    std::vector<unsigned int> nums1{ 1073741827, 52, 8831 };
-    std::vector<unsigned int> nums2{ 1073741824, 4342, 111 };
+    size_t input_1;
     std::vector<unsigned int> res;
 
     std::cout << "Input Fibonacci sequence element number: ";
     std::cin >> input_1;
+    
+    std::vector<std::vector<unsigned int>> fibonacci_sequence_elements(input_1 + 1, res);
 
-    sum(res, nums1, nums2);
-
-    for (size_t i = 0; i < 3; ++i)
-    {
-        std::cout << res[i] << " ";
-    }
-
-    std::cout << "\n";
+    fibonacci_sequence(input_1, fibonacci_sequence_elements);
 
     return 0;
 }
