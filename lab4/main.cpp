e67406a1c6c770c79b135496fc79af0e7daa90c5
dyo4 <dyo4@tpu.ru>
2024-04-20 13:51:13 +0700

    4.1

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,45 +1,40 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-
-using namespace std;
-using namespace cv;
  
 int main()
 {
-    // Reading image
-    Mat img = imread("blck.jpeg");
-    // Display original image
+    // Чтение изображения
+    cv::Mat img = cv::imread("car.jpg"); // test.jpg или car.jpg
+
+    // Вывод оригинального изображения
     imshow("original Image", img);
-    waitKey(0);
+    cv::waitKey(0);
  
-    // Convert to graycsale
-    Mat img_gray;
-    cvtColor(img, img_gray, COLOR_BGR2GRAY);
-    // Blur the image for better edge detection
-    Mat img_blur;
-    GaussianBlur(img_gray, img_blur, Size(3,3), 0);
+    // Конвертация изображения в серую палитру
+    cv::Mat img_gray;
+    cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
+
+    // Размытие изображения, для уменьшения шума и улучшения работы выделения границ
+    cv::Mat img_blur;
+    GaussianBlur(img_gray, img_blur, cv::Size(3, 3), 0);
      
     // Sobel edge detection
-    Mat sobelx, sobely, sobelxy;
-    Sobel(img_blur, sobelx, CV_64F, 1, 0, 5);
-    Sobel(img_blur, sobely, CV_64F, 0, 1, 5);
-    Sobel(img_blur, sobelxy, CV_64F, 1, 1, 5);
-    // Display Sobel edge detection images
-    imshow("Sobel X", sobelx);
-    waitKey(0);
-    imshow("Sobel Y", sobely);
-    waitKey(0);
+    cv::Mat sobelxy;
+    Sobel(img_blur, sobelxy, CV_8U, 1, 1, 5);
+
+    // Вывод изображения после Sobel edge detection
     imshow("Sobel XY using Sobel() function", sobelxy);
-    waitKey(0);
+    cv::waitKey(0);
  
     // Canny edge detection
-    Mat edges;
+    cv::Mat edges;
     Canny(img_blur, edges, 100, 200, 3, false);
-    // Display canny edge detected image
+
+    // Вывод изображения после Canny edge detection
     imshow("Canny edge detection", edges);
-    waitKey(0);
+    cv::waitKey(0);
      
-    destroyAllWindows();
+    cv::destroyAllWindows();
 
     return 0;
 }
