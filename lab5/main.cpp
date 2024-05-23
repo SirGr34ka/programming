84c3d9e7fb0b127165b791fd6e9d06811c0abbba
SirGr34ka <osipov.d.yr@gmail.com>
2024-05-23 20:28:56 +0700

    5.2 patched

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
-#include "fibonacciSequence.h"
+#include "division.h"
 
 int main(int, char**)
 {
-    size_t input_1;
-    
-    std::cout << "Input Fibonacci sequence element number: ";
-    std::cin >> input_1;
+    int32_t first, second;
 
-    fibonacci_output(input_1);
-    
+    std::cout << "Input first number: ";
+    std::cin >> first;
+
+    std::cout << "Input second number: ";
+    std::cin >> second;
+
+    // Обработка исключения
+    try
+    {
+        int32_t res = division(first, second);
+        std::cout << "Division result: " << res;
+    }
+    catch(const char* err)
+    {
+        std::cerr << err << std::endl;
+        return 1;
+    }
+
     return 0;
 }
