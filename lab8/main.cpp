188b5e0f62728c2243d37ecf85e3394fcca06d74
dyo4 <dyo4@tpu.ru>
2024-06-22 10:36:58 +0700

    8.3.1

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <string>
+#include <fstream>
+
 #include "hash_summ.h"
 
 int main( int , char** )
 {
     std::string image_path = "../kiwi.jpg";
+    std::string file_path = "../hash.txt";
+
     // Чтение изображения
     cv::Mat image = cv::imread( image_path );
 
@@ -23,10 +27,22 @@ int main( int , char** )
     cv::waitKey(0);
 
     std::string changed_image_hash = getHashSumm( image_path );
-    
+
     std::cout << "Original image hash summary: " << original_image_hash << std::endl;
     std::cout << "Changed image hash summary: " << changed_image_hash << std::endl;
     std::cout << "Are equal: " << ( original_image_hash == changed_image_hash ) << std::endl;
+
+    // Запись в файл hash.txt
+    std::ofstream file( file_path );
+    
+    if ( file.is_open() )
+    {
+        file << "Original image hash summary: " << original_image_hash << "\n";
+        file << "Changed image hash summary: " << changed_image_hash << "\n";
+        file << "Are equal: " << ( original_image_hash == changed_image_hash ) << "\n";
+    }
+
+    file.close();
      
     cv::destroyAllWindows();
 
