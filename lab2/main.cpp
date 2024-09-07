bf26a695bd8df9312600d65fa4bcf28e37a0b6bc
SirGr34ka <osipov.d.yr@gmail.com>
2024-09-07 19:21:10 +0700

    empty master

--- lab2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-int main(int, char**)
-{
-    
-
-    return 0;
-}
--- lab5/functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef functions_h
-#define functions_h
-
-// Вызов последующих функций
-void function1(const int& num);
-
-#endif
