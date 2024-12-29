d85f681c0eaa91ea4bb71f62aae17424fcbdf43b
SirGr34ka <osipov.d.yr@gmail.com>
2024-12-29 22:53:40 +0700

    linear alloc

--- allocator.h
+++ allocator.h
@@ -1,25 +1,74 @@
 #pragma once
 
-#include <corecrt.h>
+#include <iostream>
 
 template<class T>
-struct Allocator 
+class Allocator 
 {
+    const float EXPANSION_COEFFICIENT = 1.5F;
+
+    T* block_ptr;
+    size_t capacity;
+    size_t offset;
+
+    size_t type_size;
+
+    public:
+
     typedef T value_type;
 
-    Allocator(){}
+    Allocator( size_t capacity_ = 10 )
+    {
+        capacity = capacity_;
+        block_ptr = static_cast< T* >( operator new( capacity_ * sizeof( T ) ) );
+        offset = 0;
+
+        type_size = sizeof( T );
+    }
 
     template < class U > 
-    Allocator( const Allocator< U >& other ){}
+    Allocator( const Allocator< U >& other )
+    {
+        capacity = other.capacity;
+        block_ptr = other.block_ptr;
+        offset = other.offset;
+    }
 
     T* allocate( size_t count_objects )
     {
-        return static_cast< T* >( operator new( count_objects * sizeof( T ) ) );
+        if ( count_objects <= capacity - offset )
+        {
+            offset += count_objects;
+
+            return block_ptr + offset - count_objects;
+        }
+
+        size_t new_capacity = static_cast< size_t >( static_cast< float >( capacity ) * EXPANSION_COEFFICIENT );
+
+        if ( count_objects > new_capacity - offset )
+        {
+            throw std::bad_alloc();
+        }
+
+        T* new_block_ptr = static_cast< T* >( operator new( new_capacity * sizeof( T ) ) );
+
+        for ( size_t i = 0 ; i < offset ; ++i )
+        {
+            *( new_block_ptr + i ) = *( block_ptr + i );
+        }
+
+        operator delete( block_ptr , offset );
+
+        block_ptr = new_block_ptr;
+        capacity = new_capacity;
+        offset += count_objects;
+
+        return block_ptr + offset - count_objects;
     }
 
     void deallocate( T* ptr , size_t count_objects )
     {
-        operator delete( ptr , count_objects );
+        operator delete( ptr , count_objects * sizeof( T ) );
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -6,15 +6,47 @@
 #include "containers.h"
 #include "allocator.h"
 
+size_t factorial( size_t n )
+{
+    size_t result = 1;
+
+    for ( size_t i = 2 ; i <= n ; ++i )
+    {
+        result *= i;
+    }
+
+    return result;
+}
+
 int main(int, char **)
 {
     try
     {
-        std::map< int , int , std::less< int > Allocator< std::pair< int , int > > > mapchik { { 1 , 2 } , { 3 , 4 } , { 5 , 6 } };
+        const size_t MAP_SIZE = 10;
+        std::map< int , int > map1;
 
-        for ( auto iter = mapchik.begin() ; iter != mapchik.end() ; ++iter )
+        for ( size_t i = 1 ; i <= MAP_SIZE ; ++i )
         {
-            std::cout << (*iter).first << ';' << (*iter).second << ' ' << std::endl;
+            std::pair< const int , int > temp { i , static_cast< int >( factorial( i ) ) };
+            map1.insert( temp );
+        }
+
+        for ( auto iter = map1.begin() ; iter != map1.end() ; ++iter )
+        {
+            std::cout << (*iter).first << ": " << (*iter).second << std::endl;
+        }
+
+        std::map< int , int , std::less< int > , Allocator< std::pair< const int , int > > > map2;
+
+        for ( size_t i = 1 ; i <= MAP_SIZE ; ++i )
+        {
+            std::pair< const int , int > temp { i , static_cast< int >( factorial( i ) ) };
+            map2.insert( temp );
+        }
+
+        for ( auto iter = map2.begin() ; iter != map2.end() ; ++iter )
+        {
+            std::cout << (*iter).first << ": " << (*iter).second << std::endl;
         }
     }
     catch(const std::exception &e)
