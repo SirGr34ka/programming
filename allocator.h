2eec0b012a07ca9b1a4f7a681014df513dbadb35
SirGr34ka <osipov.d.yr@gmail.com>
2025-01-21 02:16:24 +0700

    shedevro pool alloc

--- allocator.h
+++ allocator.h
@@ -4,17 +4,20 @@
 #include <new> // bad_alloc, bad_array_new_length
 #include <memory>
 
+#include <tuple>
+
 template < typename T , size_t SIZE >
 struct Allocator
 {
     size_t size_in_bytes;
     std::shared_ptr< void* > pool_shared_ptr;
+    std::shared_ptr< bool[] > chunks_shared_ptr;
     
     typedef T value_type;
 
     Allocator()
     {
-        size_in_bytes = SIZE * sizeof( T );
+        size_in_bytes = ( SIZE + 2ull ) * sizeof( T );
         pool_shared_ptr = std::make_shared< void* >();
         *pool_shared_ptr = operator new( size_in_bytes );
 
@@ -22,11 +25,19 @@ struct Allocator
         {
             throw std::bad_alloc();
         }
+
+        chunks_shared_ptr = std::make_shared< bool[] >( SIZE + 2ull );
+
+        for ( size_t i = 0 ; i < SIZE ; ++i )
+        {
+            chunks_shared_ptr[ i ] = false;
+        }
     }
 
     // A converting copy constructor:
     template< typename U >
-    Allocator( const Allocator< U , SIZE >& other ) noexcept : size_in_bytes( other.size_in_bytes ) , pool_shared_ptr( other.pool_shared_ptr )
+    Allocator( const Allocator< U , SIZE >& other ) noexcept :
+    size_in_bytes( other.size_in_bytes ) , pool_shared_ptr( other.pool_shared_ptr ) , chunks_shared_ptr( other.chunks_shared_ptr )
     {
         return;
     }
@@ -77,18 +88,48 @@ T* Allocator< T , SIZE >::allocate( const size_t n )
         throw std::bad_array_new_length();
     }
 
-    void* const ptr = operator new( n * sizeof( T ) );
+    T* ptr = nullptr;
 
-    if ( !ptr )
+    size_t count = 0 , i = 0;
+
+    while ( ( count < n ) && ( i < ( SIZE + 2ull ) ) )
     {
-        throw std::bad_alloc();
+        if ( !chunks_shared_ptr[ i ] )
+        {
+            ++count;
+        }
+        else
+        {
+            count = 0;
+        }
+
+        ++i;
     }
 
-    return static_cast< T* >( ptr );
+    if ( count == n )
+    {
+        ptr = static_cast< T* >( *pool_shared_ptr ) + ( i - count );
+
+        for ( size_t j = 0 ; j < count ; ++j )
+        {
+            chunks_shared_ptr[ ( i - count ) + j ] = true;
+        }
+    }
+    else
+    {
+        throw "Out of memory!";
+    }
+
+    return ptr;
 }
 
 template< typename T , size_t SIZE >
 void Allocator< T , SIZE >::deallocate( T * const ptr , size_t n ) noexcept
 {
-    operator delete( ptr , n * sizeof( T ) );
+    size_t diff = ptr - static_cast< T* >( *pool_shared_ptr );
+
+    for ( size_t i = 0 ; i < n ; ++i )
+    {
+        chunks_shared_ptr[ diff + i ] = false;
+    }
 }
